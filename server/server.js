5b8ec6f2c30988c6c9dbd04c0b31551ccafbb01f
wale_A <isiaq.oa@gmail.com>
2017-07-18 22:07:23 +0100

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find({}).then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(e);
+    });
+});
 
 
 app.listen(3000, () => {
